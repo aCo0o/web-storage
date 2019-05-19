86d1b300d5d9512d95ea1d35500664ee5d8c19b2
Aleksandar Cincarevic <a.cincarevic@Aleksandars-MacBook-Pro.local>
2019-05-19 02:38:21 +0200

    wstorage.js

---
+++ public/js/wstorage.js
@@ -0,0 +1,10 @@
+const web_storage = {} || new Object;
+let wStore = web_storage; 
+
+wStore.storage = {
+    local : localStorage,
+    session : sessionStorage
+}
+
+window.wStore = web_storage;
+console.log(wStore);
